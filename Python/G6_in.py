923220245804b21ec59320d712c1b1ed0599572c
Evan Mackinnon <evan.mackinnon@gmail.com>
2022-03-10 01:00:26 -0800

    trying to connect to graph class

--- Python/G6_in.py
+++ Python/G6_in.py
@@ -1,12 +1,15 @@
+import itertools
+from msilib.schema import SelfReg
 import networkx as nx
 import matplotlib.pyplot as plt
-
+from Graph import *
 
 
 class G6_in():
 
     def __init__(self, file) -> None:
-        self.G = nx.read_graph6(file)
+        self.G = nx.read_graph6(file) 
+
 
     def read_file(self, file):
         self.G = nx.read_graph6(file)
@@ -23,8 +26,27 @@ class G6_in():
 
         plt.tight_layout()
         plt.show()
-    
+
+    def create_graph(self):
+        out = []
+        for g in self.G:
+            source = []
+            destination = []
+            weight = []
+            edge_list = g.edges()
+            for e in edge_list:
+                print(e[0], end=" ")
+                print(e[1])
+
+                source.append(e[0])
+                destination.append(e[1])
+                weight.append(1)
+            
+            temp_graph = Graph(source, destination, weight)
+            out.append(temp_graph)
+        return out
+
 
 
 test = G6_in('./graph_files/std_geng4_c.g6')
-test.draw_graphs()
+test.create_graph()
