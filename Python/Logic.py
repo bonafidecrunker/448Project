c6ba481b1d3c8c67e590ba1f60981216f60eaeff
Evan Mackinnon <evan.mackinnon@gmail.com>
2022-03-11 12:59:18 -0800

    Added improved p4 method

--- Python/Logic.py
+++ Python/Logic.py
@@ -1,4 +1,8 @@
 import itertools
+from re import S
+from socket import socket
+
+from numpy import sort
 
 
 class Logic:
@@ -28,7 +32,7 @@ class Logic:
             permutations = list(itertools.permutations(c))
             for p in permutations:
                 if g.has_edge(p[0], p[1]) and g.has_edge(p[1], p[2]) and g.has_edge(p[2], p[3]):
-                    if g.has_edge(p[0], p[2]) or g.has_edge(p[1], p[3]):
+                    if g.has_edge(p[0], p[2]) or g.has_edge(p[0], p[3]) or g.has_edge(p[1], p[3]):
                         # print("P4 does NOT exist")
                         break
                     else:
@@ -36,4 +40,33 @@ class Logic:
                         return True
         return False
 
+    @staticmethod
+    def has_p4_improved(g):
+        combinations = list(itertools.combinations(g, 4))
+        v0, v1, v2, v3 = 0, 0, 0, 0
+        for c in combinations:
+            #for p in permutations:
+            if g.has_edge(c[0], c[1]):
+                v0, v1 = v0 + 1, v1 + 1
+            if g.has_edge(c[1],c[2]):
+                v1, v2 = v1 + 1, v2 + 1
+            if g.has_edge(c[2],c[3]):
+                v2, v3 = v2 + 1, v3 + 1
+            if g.has_edge(c[0], c[3]):
+                v0, v3 = v0 + 1, v3 + 1
+            if g.has_edge(c[1], c[3]):
+                v1, v3 = v1 + 1, v3 + 1
+            if g.has_edge(c[0], c[2]):
+                v0, v2 = v0 + 1, v2 + 1
+                #if g.has_edge(p[0], p[1]) and g.has_edge(p[1], p[2]) and g.has_edge(p[2], p[3]):
+                #    if g.has_edge(p[0], p[2]) or g.has_edge(p[0], p[3]) or g.has_edge(p[1], p[3]):
+                        # print("P4 does NOT exist")
+                #        break
+                #    else:
+                        # print("P4 does exist")
+                #        return True
+        sorted_list = sort([v0, v1, v2, v3])
+        if sorted_list[0] == sorted_list[1] == 1 and sorted_list[2] == sorted_list[3] == 2:
+            return True
+        return False
 
--- Python/test.py
+++ Python/test.py
@@ -1,26 +1,33 @@
 from g6In import *
 from Logic import *
-
+import time
 
 def main():
-    test_file = 'graph_files/std_geng5_c.g6'
+    test_file = 'graph_files/std_geng9_c.g6'
     g = g6In(test_file)
     nx_graphs = g.create_graphs()
     counter = 1
     forbidden_graphs, non_p4_graphs = [], []
+    t0 = time.time()
     for graph in nx_graphs:
-        print("Graph # {}".format(counter))
+        #print("Graph # {}".format(counter))
         counter += 1
-        if not Logic.has_p4(graph):
+        if not Logic.has_p4_improved(graph):
             non_p4_graphs.append(graph)
         else:
             forbidden_graphs.append(graph)
-    for graph in nx_graphs:
-        print(graph.edges)
+    t1 = time.time()
+    #for graph in nx_graphs:
+    #    print(graph.edges)
     # draw_graphs(nx_graphs, 'All connected graphs of 5 nodes')
-    draw_graphs(non_p4_graphs, "Connected P4-free graphs of 5 nodes")
-    draw_graphs(forbidden_graphs, "Connected forbidden graphs of 5 nodes")
-
+    #draw_graphs(non_p4_graphs, "Connected P4-free graphs of 6 nodes")
+    #draw_graphs(forbidden_graphs, "Connected forbidden graphs of 6 nodes")
+    print("Accepted Graphs")
+    print(len(non_p4_graphs), end="\n\n")
+    print("Forbidden Graphs")
+    print(len(forbidden_graphs))
+    print("Total Completetion Time {}".format(t1-t0))
+    
 
 def draw_graphs(g, title):
     total = len(g)
