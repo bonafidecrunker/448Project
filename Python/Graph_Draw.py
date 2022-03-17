a785d7fff452872662a6a153bc1ae01fab67a1ba
Evan <evan.mackinnon@gmail.com>
2022-03-17 14:53:50 -0700

    broken leaf file input

--- Python/Graph_Draw.py
+++ Python/Graph_Draw.py
@@ -8,13 +8,14 @@ def draw_graphs(g, title):
     rows = total // cols
     rows += total % cols
     position = range(1, total + 1)
-
     fig = plt.figure(figsize=(10, 10))
     plt.title(title, pad=35, fontsize=16)
     plt.axis('off')
     for k in range(total):
         ax = fig.add_subplot(rows, cols, position[k])
         ax.set_title('Graph {}'.format(k + 1))
-        nx.draw(g[k], with_labels='True', font_color='#bab0ac', node_color='#4e79a7')
+        #nx.draw(g[k], with_labels='True', font_color='#bab0ac', node_color='#4e79a7')
+        nx.draw(g[k])
+
     plt.tight_layout()
     plt.show()
--- Python/tree_test.py
+++ Python/tree_test.py
@@ -3,12 +3,9 @@ import os
 from Graph_Draw import *
 
 
-def main(file):
-    g = TreeIn(file)
-    graphs = g.get_graphs()
-    draw_graphs(graphs, "Graphs")
-    exit(0)
-
+def main():
+    #partition(graphs)
+    graphs = load_g6_leaf(False)
 
 def load_all_graphs():
     directory = os.fsencode('tree_files_subset')
@@ -20,14 +17,52 @@ def load_all_graphs():
             g = TreeIn(temp)
             for i in g.get_graphs():
                 graphs.append(i)
+    return graphs
 
-    #print(graphs)
-    draw_graphs(graphs, "asd")
-    exit(0)
+def load_g6_leaf(bool):
+    directory = os.fsencode('leaf_files')
+    graphs = []
+    for file in os.listdir(directory):
+        filename = os.fsencode(file)
+        if filename.startswith('Tree'.encode("utf-8")):
+            temp = str(directory.decode("utf-8") + "\\" + filename.decode("utf-8"))
+            g = nx.read_graph6(temp)
+            for graph in g:
+                if isinstance(type(g), list):
+                    for g2 in graph:
+                        graphs.append(g2)
+                else:
+                    graphs.append(graph)
+        print(graphs)
+        if(bool):
+            draw_graphs(graphs, filename.decode("utf-8"))
+    return graphs
+    
+def partition(graphs):
+    for g in graphs:
+        n = len(g.nodes)
+        d = nx.diameter(g)
+        l = 0
+        for node in g.degree():
+            if node[1] == 1:
+                l += 1
+        filename = build_filename(n,d,l)
+        with open("leaf_files/"+filename, "a+") as f:
+            f.write(nx.to_graph6_bytes(g).decode("utf-8").replace(">>graph6<<",""))
 
-    print(graphs)
-    #draw_graphs(graphs,"asd")
+def build_filename(n,d,l):
+    return "Tree"+str(n)+"."+str(d)+"."+str(l)+'.g6'
 
+def count_leaves(graphs):
+    count = 0
+    graph_leaves = []
+    for g in graphs:
+        count += 1
+        degree_count = [0] * 10
+        for node in g.degree():
+            degree_count[node[1]] = degree_count[node[1]] + 1
+        graph_leaves.append((g, degree_count))
 
-main('tree_files/tree6.3.txt')
-main('tree_files/tree6.4.txt')
+    print(graph_leaves)
+main()
+exit(0)
