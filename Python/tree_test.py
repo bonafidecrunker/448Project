0f02f30d9c918347485a8ecc1932838f38a254f2
Dakota Joiner <dakota.joiner@gmail.com>
2022-04-04 09:46:47 -0700

    added extended range considerations past 4-leaf power. added documentation. removed unnecessary files and folders.

--- Python/tree_test.py
+++ Python/tree_test.py
@@ -1,56 +1,60 @@
-from functools import reduce
-import networkx as nx
-from numpy import empty
-from TreeIn import *
 import os
 from os.path import exists
 from Graph_Draw import *
 from Logic import *
-import pandas as pd
 
 
-def main(index):
-    """
-    possible degree lists for graphs of four nodes:
-    [1,1,2,2] => p4
-    [1,1,1,3] => arrow head
-    [1,2,2,3] => bow and arrow
-    [2,2,2,2] => c4 (square)
-    [2,2,3,3] => one outer edge (non chordal) removed from max clique
-    [3,3,3,3] => max clique
-    """
+def main(index, k_leaf_power):
+    # make the list of all graphs smaller if it contains a forbidden induced subgraph
 
-    four_vertex_graphs = set()
-    induced_graphs_set = set()
+    forbidden_dict = {}
 
-    four_vertex_dict = {}
+    vertex_dict = {}
     induced_graph_dict = {}
 
-    file_path = 'graph_files/std_geng4_c.g6'
+    # loads in all graphs of node number index
+    file_path = f'graph_files/std_geng{index}_c.g6'
     graphs = nx.read_graph6(file_path)
     for g in graphs:
-        four_vertex_graphs.add(g)
         key = node_degree_func(g)
         key2 = '.'.join([str(c) for c in key])
-        four_vertex_dict.setdefault(key2, []).append(g)
+        vertex_dict.setdefault(key2, []).append(g)
+        if key2 == '1.1.2.3.3' or key2 == '1.2.2.3.4' or key2 == '2.2.3.3.4':
+            print(f'{key2} is not allowed')
 
+    # for files of name Tree.n.d.l where l is index, takes the k_leaf_power of each graph up to k_leaf_power times and
+    # adds it to a set (dictionary) of computed induced graphs
     for i in range(2, index + 1):
         trees = load_all_graphs(index)
         for tree in trees:
             nbunch = get_leaf_nodes(tree)
-            temp_graph = nx.power(tree, index)
+            temp_graph = nx.power(tree, i)
             induced_graph = nx.induced_subgraph(temp_graph, nbunch)
-            if nx.is_connected(induced_graph):
-                induced_graphs_set.add(induced_graph)
+            if nx.is_connected(induced_graph) and nx.diameter(induced_graph) <= k_leaf_power:
                 key = node_degree_func(induced_graph)
                 key2 = '.'.join([str(c) for c in key])
                 induced_graph_dict.setdefault(key2, []).append(tree)
 
-    forbidden_dict = {k: v for k, v in four_vertex_dict.items() if k not in induced_graph_dict}
+    temp_forbidden_dict = {k: v for k, v in vertex_dict.items() if k not in induced_graph_dict}
+    for value in temp_forbidden_dict.values():
+        print(temp_forbidden_dict.keys())
+
+    for k, v in temp_forbidden_dict.items():
+        forbidden_dict.setdefault(k, []).append(v)
+
     print(forbidden_dict)
+    for v in temp_forbidden_dict.values():
+        draw_graphs(v)
+
 
 
 def node_degree_func(graph):
+    """
+    Returns a sorted list by the number of degrees of each node in the graph.
+
+    :param graph: a networkx graph
+    :return: sorted list with degrees of each node in graph
+    """
     node_degree = []
     for node in graph.degree():
         node_degree.append(node[1])
@@ -58,36 +62,55 @@ def node_degree_func(graph):
     return node_degree
 
 
-def get_leaf_nodes(test_graph):
+def get_leaf_nodes(graph):
+    """
+    Function to return a list of leaf nodes in a networkx graph.
+
+    :param graph: networkx graph
+    :return: list of leaf nodes
+    """
     out = []
-    for node in test_graph.degree():
+    for node in graph.degree():
         if node[1] == 1:
             out.append(node[0])
-
     return out
 
 
 def load_all_graphs(index=None):
+    """
+    Loads all graphs of given diameter index for all trees. Returns a set networkx graphs.
+
+    :param index: diameter of the tree for which to load tree files
+    :return: set of networkx graphs
+    """
     graphs = set()
     if index is not None:
-        nodes_and_diameters = [i for i in  range(2 * index + 2)]
+        nodes_and_diameters = [i for i in range(16)]  # only considering graphs and trees up to 15 nodes for now
         for nodes in nodes_and_diameters:
             for diameter in nodes_and_diameters:
                 filename_ends_with = str(nodes) + '.' + str(diameter) + '.' + str(index) + '.g6'
-                temp_graphs = load_graph(filename_ends_with)
+                temp_graphs = load_all_graphs_helper(filename_ends_with)
                 graphs.update(temp_graphs)
     else:
-        temp_graphs = load_graph()
+        temp_graphs = load_all_graphs_helper()
         graphs.update(temp_graphs)
     return graphs
 
 
-def load_graph(file_ends_with='.g6'):
+def load_all_graphs_helper(file_ends_with='.g6'):
+    """
+    Given a file containing some numbers of graphs (>= 1), load those graphs into a set.
+
+    :param file_ends_with:
+    :return: a set of graphs from the g6 file
+    """
     graphs_set = set()
     directory = os.fsdecode('leaf_files')  
     temp = str(directory) + '\Tree' + str(file_ends_with)
     if exists(temp):
         graph = nx.read_graph6(temp)
+
+        # if there is more than one graph in the file
         if type(graph) is list:
             for g in graph:
                 temp_graph = nx.Graph(g)
@@ -98,23 +121,34 @@ def load_graph(file_ends_with='.g6'):
 
 
 def partition(graphs):
+    """
+    Takes in a list of tree files and partitions the graphs in the files into files that contain only trees of the
+    specified number of nodes (n), diameter (d), and leaves (l). As trees of n.d.l are found, they are appended to the
+    output file rather than overwritten. Additionally, the trees are specifically added as g6 bytes for ease of reading
+    and comparison down the line.
+
+    :param graphs: a list of graphs
+    """
     for g in graphs:
-        n = len(g.nodes)
-        d = nx.diameter(g)
-        l = 0
+        nodes = len(g.nodes)
+        diameter = nx.diameter(g)
+        leaves = 0
         for node in g.degree():
             if node[1] == 1:
-                l += 1
-        filename = build_filename(n, d, l)
+                leaves += 1
+        filename = build_filename(nodes, diameter, leaves)
         with open("leaf_files2/"+filename, "a+") as f:
             f.write(nx.to_graph6_bytes(g).decode("utf-8").replace(">>graph6<<", ""))
 
 
-def build_filename(n, d, l):
-    return "Tree"+str(n)+"."+str(d)+"."+str(l)+'.g6'
+def build_filename(nodes, diameter, leaves):
+    return f"Tree{nodes}.{diameter}.{leaves}.g6"
 
 
-main(4)
+index = 5
+k_leaf_power = 6
+main(index, k_leaf_power)
+
 
 """
 Old code - May still use
