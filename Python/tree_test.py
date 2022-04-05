cf157ef1b99e145658b6c7eea0af066b12229bde
Dakota Joiner <dakota.joiner@gmail.com>
2022-04-05 16:35:35 -0700

    added contains_known_forbidden_subgraph, fixed remove_one_node

--- Python/tree_test.py
+++ Python/tree_test.py
@@ -1,17 +1,16 @@
 import os
 from os.path import exists
+import networkx as nx
 from Graph_Draw import *
 from Logic import *
 
 
 def main(index, k_leaf_power):
     # make the list of all graphs smaller if it contains a forbidden induced subgraph
-
-
     all_graphs_k = {}
     induced_graph_dict = {}
     all_forbidden_dict = {}
-
+    minimal_forbidden_dict = {}
 
     # loads in all graphs of node number index
     file_path = f'graph_files/std_geng{index}_c.g6'
@@ -30,50 +29,69 @@ def main(index, k_leaf_power):
     2.2.3.3.4 -> Gem
     """
     trees = load_all_graphs(index)
-    #sort trees by number of leaves
+    # sort trees by number of leaves
     print(len(trees))
-#loop starts here - k leaf power loop
+    # loop starts here - k leaf power loop
     for tree in trees:
         nbunch = get_leaf_nodes(tree)
         temp_graph = nx.power(tree, index)
         induced_graph = nx.induced_subgraph(temp_graph, nbunch)
         if nx.is_connected(induced_graph):
-            key = node_degree_func(induced_graph)
-            key2 = '.'.join([str(c) for c in key])    
-            induced_graph_dict.setdefault(key2, []).append(tree)
+            key = build_graph_key(induced_graph)
+            induced_graph_dict.setdefault(key, []).append(tree)
 
-    
-    #print(induced_graph_dict.keys())
-
+    # set like operation to build a dictionary containing the difference all_graphs - induced_graphs
     temp_forbidden_dict = {k: v for k, v in all_graphs_k.items() if k not in induced_graph_dict}
-    #print(temp_forbidden_dict.keys())
+    print(temp_forbidden_dict)
 
     for k in temp_forbidden_dict.keys():
-        #check for k_leaf_power - 1 all_forbidden_dict : if true - add here
-        all_forbidden_dict.setdefault(k_leaf_power, []).append(k)
-        #false add here
-        #add to another dict all_minimal_forbidden
+        # check for k_leaf_power - 1 all_forbidden_dict : if true - add here
+        if contains_known_forbidden_subgraph(minimal_forbidden_dict, k, index - 1):
+            all_forbidden_dict.setdefault(k_leaf_power, []).append(k)
+        # false add here
+        else:
+            minimal_forbidden_dict.setdefault(k_leaf_power, []).append(k)
+        # add to another dict all_minimal_forbidden
 
-#loop ends here
+    # loop ends here
     print(all_forbidden_dict)
+    square = nx.from_edgelist([(0, 1), (1, 2), (2, 3), (3, 0)])
+    print(minimal_forbidden_dict)
 
+
+def contains_known_forbidden_subgraph(all_forbidden_dict, graphs, index):
+    """
+    Function to check if a subgraph of each graph in graphs is contained in the list of known minimal forbidden
+    induced subgraphs for k-leaf powers.
+
+    :param all_forbidden_dict: dictionary containing all known minimal forbidden induced subgraphs
+    :param graphs: list of graphs over which to compare to graphs in all_forbidden_dict
+    :return: True if graph is found, False otherwise
+    """
+
+    # if all_forbidden_dict is empty, it cannot contain a forbidden subgraph
+    if not bool(all_forbidden_dict):
+        return False
+
+    for graph in graphs:
+        if build_graph_key(graph) in all_forbidden_dict[index]:
+            return True
+    return False
 
 
 def remove_one_node(graph):
-    #TODO
-    #Fix
     """
-    Function to remove each node once and add it to a list
-    :param graph:
+    Function that takes in a graph and removes a single node from it at a time, produces a new graph, and adds it to a
+    list of output graphs. For use with contains_known_forbidden_subgraph()
+
+    :param graph: a graph from which to remove a single node at a time
     :return: list of graphs with one node removed
     """
     graph_out_list = []
-    node_list = list(graph.nodes)
-    print(node_list)
-    for i in node_list:
-        temp_graph = graph.remove_node(i)
+    for i in graph.nodes:
+        temp_graph = nx.from_edgelist(graph.edges)
+        temp_graph.remove_node(i)
         graph_out_list.append(temp_graph)
-    print(graph_out_list)
     return graph_out_list
 
 
@@ -105,7 +123,7 @@ def get_leaf_nodes(graph):
     return out
 
 
-def load_all_graphs(index=None):
+def load_all_graphs(index=15):
     """
     Loads all graphs of given diameter index for all trees. Returns a set networkx graphs.
 
@@ -113,17 +131,17 @@ def load_all_graphs(index=None):
     :return: set of networkx graphs
     """
     graphs = set()
-    if index is not None:
-        for i in range(3, index +  1):
-            nodes_and_diameters = [i for i in range(16)]  # only considering graphs and trees up to 15 nodes for now
-            for nodes in nodes_and_diameters:
-                for diameter in nodes_and_diameters:
-                    filename_ends_with = str(nodes) + '.' + str(diameter) + '.' + str(i) + '.g6'
-                    temp_graphs = load_all_graphs_helper(filename_ends_with)
-                    graphs.update(temp_graphs)
-    else:
-        temp_graphs = load_all_graphs_helper()
-        graphs.update(temp_graphs)
+    # if index is not None:
+    for i in range(3, index):
+        nodes_and_diameters = [i for i in range(index + 1)]  # default value is 15 but can be higher if we want
+        for nodes in nodes_and_diameters:
+            for diameter in nodes_and_diameters:
+                filename_ends_with = str(nodes) + '.' + str(diameter) + '.' + str(i) + '.g6'
+                temp_graphs = load_all_graphs_helper(filename_ends_with)
+                graphs.update(temp_graphs)
+    # else:
+    #     temp_graphs = load_all_graphs_helper()
+    #     graphs.update(temp_graphs)
     return graphs
 
 
@@ -173,6 +191,10 @@ def partition(graphs):
 
 def build_filename(nodes, diameter, leaves):
     return f"Tree{nodes}.{diameter}.{leaves}.g6"
+
+
+def build_graph_key(graph):
+    return '.'.join([str(c) for c in node_degree_func(graph)]) if type(graph) != str else graph
 
 
 index = 4
